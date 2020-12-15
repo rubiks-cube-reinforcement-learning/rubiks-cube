d61268f76a4dcec9b80844b49e1d1ca86bbcf3e7
Adam Zieliński <adam@adamziel.com>
2020-12-15 21:43:01 +0100

    Updated numba code

--- code_generator/numba_cuda.py
+++ code_generator/numba_cuda.py
@@ -9,30 +9,38 @@ def indices_to_one_hot(indices):
 
 
 def generate_3cube_numba_code():
-    moves = "\n".join(['        ' + line for line in generate_moves_code()])
+    moves = "\n".join(['        %s,' % indices for (name, indices) in cube_3_permutations.values()])
     moves_indices = ", ".join([f"{spec[0]}: '{name}'" for name, spec in cube_3_permutations.items()])
     moves_names = ", ".join([f"'{name}': {spec[0]}" for name, spec in cube_3_permutations.items()])
+    fixed_cubie_moves = {k:v for k,v in cube_3_permutations.items() if k in list(Cube3().FIXED_CUBIE_OPERATIONS.keys())}
+    fixed_cubie_moves_indices = ", ".join([f"{spec[0]}: '{name}'" for name, spec in fixed_cubie_moves.items()])
+    fixed_cubie_moves_names = ", ".join([f"'{name}': {spec[0]}" for name, spec in fixed_cubie_moves.items()])
     return f'''
-from numba import cuda, float32
-import numpy as np
-import math 
+from numba import cuda
+import torch 
 
 @cuda.jit
-def apply_moves(cubes, buffer, moves):
+def apply_moves_fast(cubes, buffer, moves, recipes):
     x = cuda.grid(1)
 
-    if x >= cubes.shape[0]:
+    if x >= cubes.shape[0] or x >= moves.shape[0]:
         return
     for move in moves[x]:
         if move == 0:
             break
         for i in range(324):
             buffer[x][i] = cubes[x][i]
-{moves}
+        for i in range(324):
+            cubes[x][i] = buffer[x][recipes[move - 1][i]]
 
+MOVES_CPU_TENSOR = torch.tensor([
+{moves}
+], dtype=torch.int16)
 
 MOVES_NAMES = {{{moves_indices}}}
 MOVES_INDICES = {{{moves_names}}}
+FIXED_CUBIE_MOVES_NAMES = {{{fixed_cubie_moves_indices}}}
+FIXED_CUBIE_MOVES_INDICES = {{{fixed_cubie_moves_names}}}
 '''
 
 
